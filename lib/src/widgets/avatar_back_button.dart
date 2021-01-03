cceec961bff719a60db8b40f96a656a87f563d40
Reuben Turner <groovinchip@gmail.com>
2021-01-02 23:13:04 -0500

    update docs for avatar_back_button

--- lib/src/widgets/avatar_back_button.dart
+++ lib/src/widgets/avatar_back_button.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 
-/// This widget creates a back button with a user Avatar next to it, like WhatsApp does.
-/// Given that user avatar's generally are retrieved via network calls this widget takes a String URL as the avatar and
-/// passes it to a NetworkImage widget.
+/// A back button with a user Avatar built into it (like WhatsApp).
+///
+/// Given that user avatar's generally are retrieved via network calls, this
+/// widget takes a String URL as the avatar and passes it to a NetworkImage widget.
 class AvatarBackButton extends StatelessWidget {
-  /// The string version of the URL for the user's avatar
-  final String avatar;
-
-  /// This function is called when the button is pressed.
-  final GestureTapCallback onPressed;
-
   const AvatarBackButton({
     Key key,
     this.avatar,
     this.onPressed,
   }) : super(key: key);
+
+  /// The string version of the URL for the user's avatar
+  final String avatar;
+
+  /// This function is called when the button is pressed.
+  final GestureTapCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +27,10 @@ class AvatarBackButton extends StatelessWidget {
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Icon(Icons.arrow_back, color: Colors.white),
+          Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
           CircleAvatar(
             backgroundImage: NetworkImage(avatar),
           ),

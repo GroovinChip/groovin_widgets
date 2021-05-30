f32b282805483f965e906a2db0832789bdeb2083
Reuben Turner <groovinchip@gmail.com>
2021-05-30 09:14:35 -0400

    Version 2.1 - ScrollControllerBuilder

--- lib/groovin_widgets.dart
+++ lib/groovin_widgets.dart
@@ -2,14 +2,14 @@
 
 library groovin_widgets;
 
+// Utilities
+export 'src/utils/color_utils.dart';
+export 'src/utils/json_utils.dart';
 // Widgets
+export 'src/widgets/avatar_back_button.dart';
+export 'src/widgets/groovin_expansion_tile.dart';
+export 'src/widgets/modal_drawer_handle.dart';
 export 'src/widgets/outline_dropdown_button.dart';
 export 'src/widgets/outline_dropdown_button_form_field.dart';
-export 'src/widgets/modal_drawer_handle.dart';
-export 'src/widgets/groovin_expansion_tile.dart';
+export 'src/widgets/scroll_controller_builder.dart';
 export 'src/widgets/split_color_background.dart';
-export 'src/widgets/avatar_back_button.dart';
-
-// Utilities
-export 'src/utils/json_utils.dart';
-export 'src/utils/color_utils.dart';
